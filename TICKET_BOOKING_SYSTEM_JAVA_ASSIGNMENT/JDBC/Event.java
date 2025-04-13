7acbed5e38a3e30593c0ae452de0ad71125cb68a
PAVITHRA B <pavithrabala2203@gmail.com>
2025-04-13 20:48:03 +0530

    Add files via upload

---
+++ TICKET_BOOKING_SYSTEM_JAVA_ASSIGNMENT/JDBC/Event.java
@@ -0,0 +1,137 @@
+package com.hexaware.tbs.JDBC;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Event {
+	private String eventName;
+	private LocalDate eventDate;
+    private LocalTime eventTime;
+    private String venueName;
+    private int totalSeats;
+    private int availableSeats;
+    private double ticketPrice;
+    private String eventType;
+    
+    
+    
+	public Event(String eventName, LocalDate eventDate, LocalTime eventTime, String venueName, int totalSeats,
+			int availableSeats, double ticketPrice, String eventType) {
+		super();
+		this.eventName = eventName;
+		this.eventDate = eventDate;
+		this.eventTime = eventTime;
+		this.venueName = venueName;
+		this.totalSeats = totalSeats;
+		this.availableSeats = availableSeats;
+		this.ticketPrice = ticketPrice;
+		this.eventType = eventType;
+	}
+
+
+
+	public String getEventName() {
+		return eventName;
+	}
+
+
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+
+
+	public LocalDate getEventDate() {
+		return eventDate;
+	}
+
+
+
+	public void setEventDate(LocalDate eventDate) {
+		this.eventDate = eventDate;
+	}
+
+
+
+	public LocalTime getEventTime() {
+		return eventTime;
+	}
+
+
+
+	public void setEventTime(LocalTime eventTime) {
+		this.eventTime = eventTime;
+	}
+
+
+
+	public String getVenueName() {
+		return venueName;
+	}
+
+
+
+	public void setVenueName(String venueName) {
+		this.venueName = venueName;
+	}
+
+
+
+	public int getTotalSeats() {
+		return totalSeats;
+	}
+
+
+
+	public void setTotalSeats(int totalSeats) {
+		this.totalSeats = totalSeats;
+	}
+
+
+
+	public int getAvailableSeats() {
+		return availableSeats;
+	}
+
+
+
+	public void setAvailableSeats(int availableSeats) {
+		this.availableSeats = availableSeats;
+	}
+
+
+
+	public double getTicketPrice() {
+		return ticketPrice;
+	}
+
+
+
+	public void setTicketPrice(double ticketPrice) {
+		this.ticketPrice = ticketPrice;
+	}
+
+
+
+	public String getEventType() {
+		return eventType;
+	}
+
+
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Event [eventName=" + eventName + ", eventDate=" + eventDate + ", eventTime=" + eventTime
+				+ ", venueName=" + venueName + ", totalSeats=" + totalSeats + ", availableSeats=" + availableSeats
+				+ ", ticketPrice=" + ticketPrice + ", eventType=" + eventType + "]";
+	}
+
+
+}
